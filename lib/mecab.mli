6fccd6ab375b51c2bfd28e985571bec9d3670c76
taks <857tn859@gmail.com>
2012-05-21 08:26:58 +0900

    add methods for mecab_node

--- lib/mecab.mli
+++ lib/mecab.mli
@@ -10,7 +10,29 @@ external nbest_init : t -> string -> unit = "ocaml_mecab_nbest_init"
 external nbest_init2 : t -> string -> unit = "ocaml_mecab_nbest_init2"
 external nbest_next_tostr : t -> string -> string = "ocaml_mecab_nbest_next_tostr"
 
-type node
+type node = {
+  surface : string;
+  feature : string;
+  id : int;
+  rc_attr : int;
+  lc_attr : int;
+  posid : int;
+  char_type : int;
+  stat : int;
+  isbest : bool;
+  alpha : float;
+  beta : float;
+  prob : float;
+  wcost : int;
+  cost : int;
+  prev : node option;
+  next : node option;
+}
+
+external sparse_tonode : t -> string -> node = "ocaml_mecab_sparse_tonode"
+external sparse_tonode2 : t -> string -> int -> node = "ocaml_mecab_sparse_tonode2"
+
+val each_node : node -> (node -> unit) -> unit
 
 (*** 辞書関連 ***)
 type dic_type =
--- lib/mecab_stubs.c
+++ lib/mecab_stubs.c
@@ -4,6 +4,34 @@
 #include <caml/fail.h>
 #include <caml/alloc.h>
 #include <caml/memory.h>
+#include <string.h>
+
+/*** util ***/
+static inline value
+copy_string2( const char *str, const int len ) {
+    CAMLparam0();
+    CAMLlocal1(result);
+
+    result = caml_alloc_string(len);
+    memcpy(String_val(result), str, len);
+
+    CAMLreturn(result);
+}
+
+/** option **/
+#define Val_none Val_int(0)
+
+static inline value
+Val_some( value v )
+{
+    CAMLparam1( v );
+    CAMLlocal1( some );
+    some = caml_alloc(1, 0);
+    Store_field( some, 0, v );
+    CAMLreturn( some );
+}
+
+/*** mecab ***/
 
 #define Mecab_val(x) (*((mecab_t **) Data_custom_val(x)))
 
@@ -167,6 +195,91 @@ ocaml_mecab_nbest_next_tostr( value mecab_v ) {
 }
 
 /*** 辞書関連 ***/
+/*** mecab_node_t ***/
+
+static value
+copy_mecab_node_t2( const mecab_node_t *node ,
+                    value prev_node_v, value next_node_v ) {
+    CAMLparam2(prev_node_v, next_node_v);
+    CAMLlocal1(result);
+
+    result = caml_alloc(16, 0);
+    Store_field(result,  0, copy_string2(node->surface, node->length));
+    Store_field(result,  1, caml_copy_string(node->feature));
+    Store_field(result,  2, Val_int(node->id));
+    Store_field(result,  3, Val_int(node->rcAttr));
+    Store_field(result,  4, Val_int(node->lcAttr));
+    Store_field(result,  5, Val_int(node->posid));
+    Store_field(result,  6, Val_int(node->char_type));
+    Store_field(result,  7, Val_int(node->stat));
+    Store_field(result,  8, Val_bool(node->isbest));
+    Store_field(result,  9, caml_copy_double(node->alpha));
+    Store_field(result, 10, caml_copy_double(node->beta));
+    Store_field(result, 11, caml_copy_double(node->prob));
+    Store_field(result, 12, Val_int(node->wcost));
+    Store_field(result, 13, Val_int(node->cost));
+    Store_field(result, 14, prev_node_v);
+    Store_field(result, 15, next_node_v);
+
+    CAMLreturn(result);
+}
+
+static inline void
+set_next_node( value node_v, value next_node_v ) {
+  Store_field(node_v, 15, next_node_v);
+}
+
+static value
+copy_mecab_node_t( mecab_node_t *node ) {
+  CAMLparam0();
+  CAMLlocal3(prev_node_v, node_v, start_node_v);
+
+  start_node_v = copy_mecab_node_t2(node, Val_none, Val_none);
+  node_v = start_node_v;
+
+  while(1) {
+    prev_node_v = node_v;
+    node = node->next;
+    if(node == NULL) { break; }
+    node_v = copy_mecab_node_t2(node, Val_some(prev_node_v), Val_none);
+    set_next_node(prev_node_v, Val_some(node_v));
+  }
+
+  CAMLreturn(start_node_v);
+}
+
+CAMLprim value
+ocaml_mecab_sparse_tonode( value mecab_v, value str_v ) {
+    CAMLparam2(mecab_v, str_v);
+
+    mecab_t *mecab = Mecab_val(mecab_v);
+
+    const mecab_node_t *node = mecab_sparse_tonode(mecab, String_val(str_v));
+
+    if(node == NULL) {
+        const char *err_str = mecab_strerror(mecab);
+        caml_failwith(err_str);
+    }
+
+    CAMLreturn(copy_mecab_node_t(node));
+}
+
+CAMLprim value
+ocaml_mecab_sparse_tonode2( value mecab_v, value str_v, value len_v ) {
+    CAMLparam3(mecab_v, str_v, len_v);
+
+    mecab_t *mecab = Mecab_val(mecab_v);
+
+    const mecab_node_t *node = mecab_sparse_tonode2(mecab, String_val(str_v), Int_val(len_v));
+
+    if(node == NULL) {
+        const char *err_str = mecab_strerror(mecab);
+        caml_failwith(err_str);
+    }
+
+    CAMLreturn(copy_mecab_node_t(node));
+}
+
 static inline value ocaml_dict( const mecab_dictionary_info_t *dict ) {
     CAMLparam0();
     CAMLlocal1(result);

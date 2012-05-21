e3d74d20b81b65f35e6bf16399d89bf4b2fd6e29
taks <857tn859@gmail.com>
2012-05-21 20:54:36 +0900

    modify example file

--- example/example.ml
+++ example/example.ml
@@ -3,12 +3,9 @@
 #load "mecab.cma"
 
 let () =
-  let m = Mecab.init "-Ochasen" in
-  print_endline (Mecab.sparse_tostr m "今日もしないとね")
-;;
-
-let () =
   let m = Mecab.init "" in
+  print_endline (Mecab.sparse_tostr m "今日もしないとね");
+
   Mecab.each_node (Mecab.sparse_tonode m "今日もしないとね")
-    (fun x ->  print_endline x.Mecab.feature)
+    (fun x ->  print_endline x.Mecab.feature);
 ;;
